5659166fb6cec4c70473276d082c013067142d2e
Giovanni Harold <gharold@2ndwatch.com>
2023-04-28 15:44:08 -0500

    dropping header

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
     # Can be used wherever a "file-like" object is accepted:
     # columns = ['location', 'campaign', 'spend_date', 'spend']
-    dataframe = pd.read_csv(uploaded_file, skiprows=1)
+    dataframe = pd.read_csv(uploaded_file)
     dataframe.columns = [it.upper() for it in dataframe.columns]    
     st.write(dataframe)
     if st.button(label='Upload File'):
