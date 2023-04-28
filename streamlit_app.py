cc6cd6397491c2a9bd0e4bba59729b33c7a5a731
Giovanni Harold <104081304+gharold13@users.noreply.github.com>
2023-04-28 12:54:36 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,7 @@ import pandas as pd
 import streamlit as st
 import snowflake.connector
 import sys
+from io import StringIO
 
 """
 # Welcome to Streamlit!
@@ -28,6 +29,25 @@ def run_query(query):
         dat = cur.fetchall()
         df = pd.DataFrame(dat, columns=[col[0] for col in cur.description])
         return df
+    
+uploaded_file = st.file_uploader("Choose a file")
+
+if uploaded_file is not None:
+    # To read file as bytes:
+    bytes_data = uploaded_file.getvalue()
+    st.write(bytes_data)
+
+    # To convert to a string based IO:
+    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+    st.write(stringio)
+
+    # To read file as string:
+    string_data = stringio.read()
+    st.write(string_data)
+
+    # Can be used wherever a "file-like" object is accepted:
+    dataframe = pd.read_csv(uploaded_file)
+    st.write(dataframe)
 
 #df = run_query("SELECT * from FOOD_INSPECTIONS_FULL")
 
