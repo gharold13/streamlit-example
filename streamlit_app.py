6aedb241b56be41f2cdef586f6d8a66bac82e2cb
Giovanni Harold <gharold@2ndwatch.com>
2023-04-28 15:45:47 -0500

    column name

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,9 +36,9 @@ uploaded_file = st.file_uploader("Choose a file")
 
 if uploaded_file is not None:
     # Can be used wherever a "file-like" object is accepted:
-    # columns = ['location', 'campaign', 'spend_date', 'spend']
-    dataframe = pd.read_csv(uploaded_file)
-    dataframe.columns = [it.upper() for it in dataframe.columns]    
+    columns = ['LOCATION', 'CAMPAIGN', 'SPEND_DATE', 'SPEND']
+    dataframe = pd.read_csv(uploaded_file, columns=columns)
+    # dataframe.columns = [it.upper() for it in dataframe.columns]    
     st.write(dataframe)
     if st.button(label='Upload File'):
         write_pandas(conn, 
